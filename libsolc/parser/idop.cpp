0eef69a5ff164ab1339edeb9229ed85a8f5c1f98
Nyvyme <mixagame228@gmail.com>
2025-10-31 18:06:01 +0500

    parser: Change behaviour of Parser::parse_identifier_operand for access members.

--- libsolc/parser/idop.cpp
+++ libsolc/parser/idop.cpp
@@ -1,19 +1,9 @@
-#include "libsolc/logger.hpp"
 #include "libsolc/parser/macros.hpp"
 #include "parser/parser.hpp"
-#include <iterator>
 #include <vector>
 
 namespace solc
 {
-
-template <typename T>
-static inline void erase_after(std::vector<T> &v,
-                               typename std::vector<T>::size_type len)
-{
-  if (len > v.size())
-    v.erase(v.begin() + len, v.end());
-}
 
 AST Parser::parse_identifier_operand(bool accept_modules, bool accept_functions)
 {
@@ -47,17 +37,11 @@ AST Parser::parse_identifier_operand(bool accept_modules, bool accept_functions)
   if (next == TokenType::PERIOD) {
     _pos++;
     VERIFY_POS(_pos);
-    auto symbol = parse_identifier_operand(accept_modules, accept_functions);
+    auto symbol = parse_identifier_operand(false, accept_functions);
 
-    if (out_operand.type == ASTType::EXPR_OPERAND_CALL ||
-        out_operand.type == ASTType::EXPR_OPERAND_GENERIC_CALL) {
-      AST access(_pos - 1, ASTType::EXPR_OPERAND_ACCESS_MEMBER);
-      access.append(symbol);
-      out_operand.append(access);
-    } else {
-      out_operand.type = ASTType::EXPR_OPERAND_ACCESS_MEMBER;
-      out_operand.append(symbol);
-    }
+    AST access(_pos - 1, ASTType::EXPR_OPERAND_ACCESS_MEMBER);
+    access.append(symbol);
+    out_operand.append(access);
   }
 
   return out_operand;
