e2d430f789692f3f633bb3294b1f7aa74343230c
Nyvyme <mixagame228@gmail.com>
2026-03-04 06:13:55 +0500

    parser: Add _INCOMPLETE_ functions to parse... functions :^)

---
+++ libsolc/parser/parser/func.c
@@ -0,0 +1,13 @@
+#include "solc/defs.h"
+#include "solc/parser/ast.h"
+#include "solc/parser/parser.h"
+
+solc_ast_t *solc_parser_parse_def_func(solc_parser_t *parser)
+{
+  SOLC_TODO("Parse function definition.");
+}
+
+solc_ast_t *solc_parser_parse_def_func_generic(solc_parser_t *parser)
+{
+  SOLC_TODO("Parse generic function definition.");
+}
