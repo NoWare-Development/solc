ff71b52dbda19c0463597720065ae2cee16aee38
Nyvyme <mixagame228@gmail.com>
2026-03-08 04:09:04 +0500

    parser: Parse for statement.

--- libsolc/parser/parser_context.c
+++ libsolc/parser/parser_context.c
@@ -40,7 +40,7 @@ void parser_context_initialize(void)
   trie_insert(parser_ctx.stmt_funcptrs, "continue",
               solc_parser_parse_stmt_continue);
   trie_insert(parser_ctx.stmt_funcptrs, "while", solc_parser_parse_stmt_while);
-  // trie_insert(parser_ctx.stmt_funcptrs, "for", solc_parser_parse_stmt_for);
+  trie_insert(parser_ctx.stmt_funcptrs, "for", solc_parser_parse_stmt_for);
   trie_insert(parser_ctx.stmt_funcptrs, "do", solc_parser_parse_stmt_dowhile);
   // trie_insert(parser_ctx.stmt_funcptrs, "switch", solc_parser_parse_stmt_switch);
   trie_insert(parser_ctx.stmt_funcptrs, "defer", solc_parser_parse_stmt_defer);
