0baa8da6da4671316582ed7b2bf52ebd5f5cc125
Nyvyme <mixagame228@gmail.com>
2026-04-08 09:15:05 +0500

    solc: Make compiler version a string.

--- src/main.c
+++ src/main.c
@@ -16,19 +16,19 @@
 #include <stdlib.h>
 #include <solc/parser/ast.h>
 
-#define SOLC_VERSION 0.0.1
+#define SOLC_VERSION "0.0.1"
 #define SOLC_COPYRIGHT_YEAR 2026
 #define SOLC_COMPILER_DEVELOPER "NoWare-Development"
 
-static const char *_version_message = "solc (SOLC) " __SOLC_MACRO_STR(
-  SOLC_VERSION) "\n"
-                "Copyright (C) " __SOLC_MACRO_STR(
-                  SOLC_COPYRIGHT_YEAR) " " SOLC_COMPILER_DEVELOPER "\n"
-                                       "This is free software; see the source "
-                                       "for copying conditions (not ready yet)."
-                                       "  There is NO\n"
-                                       "warranty; not even for MERCHANTABILITY"
-                                       " or FITNESS FOR A PARTICULAR PURPOSE.";
+static const char *_version_message =
+  "solc (SOLC) " SOLC_VERSION "\n"
+  "Copyright (C) " __SOLC_MACRO_STR(
+    SOLC_COPYRIGHT_YEAR) " " SOLC_COMPILER_DEVELOPER "\n"
+                         "This is free software; see the source "
+                         "for copying conditions (not ready yet)."
+                         "  There is NO\n"
+                         "warranty; not even for MERCHANTABILITY"
+                         " or FITNESS FOR A PARTICULAR PURPOSE.";
 
 s32 main(s32 argc, char **argv)
 {
